713a186b10542db74a4d87217016134b118b7701
Liam Leirs <liamleirs@hotmail.com>
2022-03-06 16:27:31 +0100

    Added NFA Class

---
+++ NFA.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Liam on 6/03/2022.
+//
+
+#include "NFA.h"
---
+++ NFA.h
@@ -0,0 +1,17 @@
+//
+// Created by Liam on 6/03/2022.
+//
+
+#ifndef NFA_NFA_H
+#define NFA_NFA_H
+
+
+class NFA {
+public:
+
+private:
+
+};
+
+
+#endif //NFA_NFA_H
